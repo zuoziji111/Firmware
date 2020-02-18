e1a56b8137dc782b19053b04dabf15e532ddd008
Daniel Agar <daniel@agar.ca>
2020-02-18 13:33:18 -0500

    uORB::SubscriptionCallback relax interval check to accommodate realistic sensor publication rates and occasional jitter

--- src/modules/uORB/SubscriptionCallback.hpp
+++ src/modules/uORB/SubscriptionCallback.hpp
@@ -128,7 +128,8 @@ public:
 	void call() override
 	{
 		// schedule immediately if no interval, otherwise check time elapsed
-		if ((_interval_us == 0) || (hrt_elapsed_time_atomic(&_last_update) >= _interval_us)) {
+		// interval check is relaxed to accommodate realistic sensor publication rate differences and occasional jitter
+		if ((_interval_us == 0) || (hrt_elapsed_time_atomic(&_last_update) >= (0.95f * _interval_us))) {
 			_work_item->ScheduleNow();
 		}
 	}
